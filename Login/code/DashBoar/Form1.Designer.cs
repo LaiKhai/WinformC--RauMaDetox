c8e15d4a0e711a7e3a37f1c4bde05255128d07dc
Lai Khải <56122788+LaiKhai@users.noreply.github.com>
2021-02-16 17:12:56 +0700

    Form chức năng
    Chờ làm các chức năng

---
+++ Login/code/DashBoar/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace DashBoar
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.EditProfile = new System.Windows.Forms.Label();
+            this.UserName2 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.UserPic2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UserPic2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(259, 713);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn6
+            // 
+            this.btn6.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.btn6.FlatAppearance.BorderSize = 0;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.Font = new System.Drawing.Font("Russo One", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.ForeColor = System.Drawing.Color.White;
+            this.btn6.Location = new System.Drawing.Point(0, 669);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(259, 42);
+            this.btn6.TabIndex = 8;
+            this.btn6.Text = "Setting";
+            this.btn6.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.panel2.Controls.Add(this.EditProfile);
+            this.panel2.Controls.Add(this.UserName2);
+            this.panel2.Controls.Add(this.UserPic2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(259, 202);
+            this.panel2.TabIndex = 1;
+            // 
+            // EditProfile
+            // 
+            this.EditProfile.AutoSize = true;
+            this.EditProfile.Font = new System.Drawing.Font("Russo One", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditProfile.ForeColor = System.Drawing.Color.White;
+            this.EditProfile.Location = new System.Drawing.Point(75, 164);
+            this.EditProfile.Name = "EditProfile";
+            this.EditProfile.Size = new System.Drawing.Size(105, 18);
+            this.EditProfile.TabIndex = 2;
+            this.EditProfile.Text = "Edit Profiles";
+            // 
+            // UserName2
+            // 
+            this.UserName2.AutoSize = true;
+            this.UserName2.Font = new System.Drawing.Font("Russo One", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserName2.ForeColor = System.Drawing.Color.White;
+            this.UserName2.Location = new System.Drawing.Point(66, 135);
+            this.UserName2.Name = "UserName2";
+            this.UserName2.Size = new System.Drawing.Size(124, 25);
+            this.UserName2.TabIndex = 1;
+            this.UserName2.Text = "User Name";
+            // 
+            // btn2
+            // 
+            this.btn2.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.btn2.FlatAppearance.BorderSize = 0;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Russo One", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.White;
+            this.btn2.Image = ((System.Drawing.Image)(resources.GetObject("btn2.Image")));
+            this.btn2.Location = new System.Drawing.Point(0, 261);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(259, 42);
+            this.btn2.TabIndex = 7;
+            this.btn2.Text = "Home";
+            this.btn2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.btn4.FlatAppearance.BorderSize = 0;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Russo One", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.ForeColor = System.Drawing.Color.White;
+            this.btn4.Image = ((System.Drawing.Image)(resources.GetObject("btn4.Image")));
+            this.btn4.Location = new System.Drawing.Point(0, 382);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(259, 42);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "Home";
+            this.btn4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.btn3.FlatAppearance.BorderSize = 0;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Russo One", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.ForeColor = System.Drawing.Color.White;
+            this.btn3.Image = ((System.Drawing.Image)(resources.GetObject("btn3.Image")));
+            this.btn3.Location = new System.Drawing.Point(0, 320);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(259, 42);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "Home";
+            this.btn3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.btn5.FlatAppearance.BorderSize = 0;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("Russo One", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.ForeColor = System.Drawing.Color.White;
+            this.btn5.Image = ((System.Drawing.Image)(resources.GetObject("btn5.Image")));
+            this.btn5.Location = new System.Drawing.Point(0, 442);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(259, 42);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "Home";
+            this.btn5.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.btn1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn1.FlatAppearance.BorderSize = 0;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Russo One", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.White;
+            this.btn1.Image = ((System.Drawing.Image)(resources.GetObject("btn1.Image")));
+            this.btn1.Location = new System.Drawing.Point(0, 202);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(259, 46);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Home";
+            this.btn1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // UserPic2
+            // 
+            this.UserPic2.BackColor = System.Drawing.Color.Transparent;
+            this.UserPic2.BackgroundImage = global::DashBoar.Properties.Resources.LogoUser_DashBoar1;
+            this.UserPic2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.UserPic2.Location = new System.Drawing.Point(66, 12);
+            this.UserPic2.Name = "UserPic2";
+            this.UserPic2.Size = new System.Drawing.Size(122, 118);
+            this.UserPic2.TabIndex = 0;
+            this.UserPic2.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1099, 713);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UserPic2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox UserPic2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label EditProfile;
+        private System.Windows.Forms.Label UserName2;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn5;
+        public System.Windows.Forms.Button btn6;
+    }
+}
+
