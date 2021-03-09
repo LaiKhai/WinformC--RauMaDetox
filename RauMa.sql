176e8ed2a72f5063a2e845621dfb43af14fdbc93
Lai Khải <56122788+LaiKhai@users.noreply.github.com>
2021-03-09 23:14:12 +0700

    Hoan Chinh Database
    Coi Lại cần sửa cái nào thì sửa nhá

--- RauMa.sql
+++ RauMa.sql
@@ -23,12 +23,11 @@ log on
 
 ----- Tạo Bảng ----
 
--- Tài Khoản, Phân quyền --
 create table SanPham
 (
-MaSp int not null primary key,
-TenSP int,
-MaLoaiSP int ,
+MaSp char(3) not null primary key,
+TenSp int,
+MaLoaiSp char(3) ,
 GiaTien money,
 MoTa nvarchar(20),
 TrangThai bit default 1
@@ -36,22 +35,53 @@ TrangThai bit default 1
 
 create table LoaiSP
 (
-MaLoaiSP int primary key,
+MaLoaiSp char(3) primary key not null,
 LoaiSP nvarchar(20),
 TrangThai bit default 1
 )
 
 create table NhanVien
 (
-ID int not null,
+ID_NV char(3) not null,
 HoTen nvarchar(50),
 NgaySinh datetime,
+GioiTinh nvarchar(10),
 ChucDanh nvarchar(30),
 LoaiNV nvarchar(20),
 SDT char(10),
 TaiKhoan varchar(100),
+MatKhau varchar(100),
 Email varchar(100),
 TrangThai Bit default 1,
+)
+create table CT_PhanQuyen
+(
+	ID_NV char(3),
+	ID_quyen char(3),
+	Taikhoan varchar(100),
+	LoaiQuyen varchar(50),
+	TrangThai Bit default 1
+)
+create table PhanQuyen
+(
+	ID_quyen char(3) not null,
+	LoaiQuyen varchar(50)
+)
+
+create table HoaDon
+(
+	ID_HoaDon char(3) not null,
+	ID_NV char(3),
+	NgayLapHoaDon datetime,
+)
+create table CT_HoaDon
+(
+	ID_HoaDon char(3),
+	MaSp char(3),
+	SoLuong int,
+	DonGia money,
+	ChietKhau money,
+	TongTien money
 )
 
 
@@ -62,4 +92,25 @@ alter table SanPham
 add constraint FK_SanPham_LoaiSanPham foreign key (MaLoaiSP) references LoaiSP(MaLoaiSP)
 
 alter table NhanVien
-add constraint FK_NhanVien primary key (ID)
+add constraint PK_NhanVien primary key (ID_NV)
+
+alter table PhanQuyen
+add constraint PK_PhanQuyen primary key (ID_quyen)
+
+alter table HoaDon
+add constraint PK_HoaDon primary key (ID_HoaDon)
+
+alter table CT_PhanQuyen
+add constraint FK_CTPhanQuyen_PhanQuyen foreign key (ID_quyen) references PhanQuyen(ID_quyen)
+
+alter table CT_PhanQuyen
+add constraint FK_CTPhanQuyen_NhanVien foreign key (ID_NV) references NhanVien(ID_NV)
+
+alter table CT_HoaDon
+add constraint FK_CTHoaDon_HoaDon foreign key (ID_HoaDon) references HoaDon(ID_HoaDon)
+
+alter table CT_HoaDon
+add constraint FK_CTHoaDon_SanPham foreign key (MaSp) references SanPham(MaSp)
+
+alter table HoaDon
+add constraint FK_HoaDon_NhanVien foreign key (ID_NV) references NhanVien(ID_NV)
